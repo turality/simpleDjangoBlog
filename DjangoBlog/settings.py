9d3c0cacd9c27afd95064e5ecd9ccc5103407915
Tural Muradli <muradlitural799@gmail.com>
2022-06-01 15:23:40 +0400

    changed settings.py file

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yq*=nl@tmlmq9#bp_m^qvn5q4=4e^%@h^q%sh+bp-cnyu1%yd_'
-
+#SECRET_KEY = 'django-insecure-yq*=nl@tmlmq9#bp_m^qvn5q4=4e^%@h^q%sh+bp-cnyu1%yd_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['turalsdjangoapp.herokuapp.com']
 
 
 # Application definition
